937cf76ac58562de53b5e737f692643618559773
Silvia Colmenares <Silvianohemy38@gmail.com>
2022-06-04 19:18:19 -0300

    mejorando proyecto y agregando storage

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,8 @@
-const meterArray = (tipoProducto) => tipoProducto.split("/");
-let divProductos = document.getElementById("productos");
+/*Codigo repetido de codigoDeVender.js para que el carrito funcione en index */
+
 let carrito = document.getElementsByClassName("carrito__icono");
+const meterArray = (tipoProducto) => tipoProducto.split("/");
+
 
 class productos{
     constructor(imagen,marca,titulo,precio,tipo,id){
@@ -14,58 +16,25 @@ class productos{
 }
 
 const listaDeProductos = [
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio FX-991SPX II",32.89,meterArray("cientifica"),1), 
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio FX-82MS",16.80,meterArray("cientifica"),2), 
-    new productos("../imagenes/Calculator-bro.png","Canon","Canon MP1211-LTSC",76.06,meterArray("impresora integrada"),3),
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio FR-2650",50.08,meterArray("impresora integrada"),4),
-    new productos("../imagenes/Calculator-bro.png","Canon","Canon TX-220TSII",15.00,meterArray("estandar"),5),
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio HL-815",9.37,meterArray("bolsillo"),6),
-    new productos("../imagenes/Calculator-bro.png","Olivetti","Olivetti Logos 902",79.00,meterArray("impresora integrada"),7),
-    new productos("../imagenes/Calculator-bro.png","Olivetti","Olivetti Summa 60",40.00,meterArray("mesa"),8),
-    new productos("../imagenes/Calculator-bro.png","Olivetti","Olivetti Summa 301",15.05,meterArray("impresora integrada/bolsillo"),9),
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio FX 260 Solar II",16.80,meterArray("cientifica/solar"),10), 
-    new productos("../imagenes/Calculator-bro.png","Casio","CASIO PRIZM FX-CG50",80.99,meterArray("cientifica/grafica"),11), 
-    new productos("../imagenes/Calculator-bro.png","Ibico","Ibico 081X",15.99,meterArray("bolsillo/estandar"),12), 
-    new productos("../imagenes/Calculator-bro.png","Ibico","Ibico 212X",23.78,meterArray("mesa"),13), 
-    new productos("../imagenes/Calculator-bro.png","Ibico","Ibico 1491X",83.78,meterArray("impresora integrada"),14),
-    new productos("../imagenes/Calculator-bro.png","Hp","HP 2AP18AA#ABA",129.00,meterArray("cientifica/grafica"),15), 
-    new productos("../imagenes/Calculator-bro.png","Hp","HP 50g",321.00,meterArray("cientifica/grafica"),16),
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio FX-9750gIII",56.08,meterArray("cientifica/grafica/solar"),17), 
-    new productos("../imagenes/Calculator-bro.png","Casio","Casio FX 260 Solar II",56.08,meterArray("cientifica/solar"),18), 
+    new productos("imagenes/Calculator-bro.png","Casio","Casio FX-991SPX II",32.89,meterArray("cientifica"),1), 
+    new productos("imagenes/Calculator-bro.png","Casio","Casio FX-82MS",16.80,meterArray("cientifica"),2), 
+    new productos("imagenes/Calculator-bro.png","Canon","Canon MP1211-LTSC",76.06,meterArray("impresora integrada"),3),
+    new productos("imagenes/Calculator-bro.png","Casio","Casio FR-2650",50.08,meterArray("impresora integrada"),4),
+    new productos("imagenes/Calculator-bro.png","Canon","Canon TX-220TSII",15.00,meterArray("estandar"),5),
+    new productos("imagenes/Calculator-bro.png","Casio","Casio HL-815",9.37,meterArray("bolsillo"),6),
+    new productos("imagenes/Calculator-bro.png","Olivetti","Olivetti Logos 902",79.00,meterArray("impresora integrada"),7),
+    new productos("imagenes/Calculator-bro.png","Olivetti","Olivetti Summa 60",40.00,meterArray("mesa"),8),
+    new productos("imagenes/Calculator-bro.png","Olivetti","Olivetti Summa 301",15.05,meterArray("impresora integrada/bolsillo"),9),
+    new productos("imagenes/Calculator-bro.png","Casio","Casio FX 260 Solar II",16.80,meterArray("cientifica/solar"),10), 
+    new productos("imagenes/Calculator-bro.png","Casio","CASIO PRIZM FX-CG50",80.99,meterArray("cientifica/grafica"),11), 
+    new productos("imagenes/Calculator-bro.png","Ibico","Ibico 081X",15.99,meterArray("bolsillo/estandar"),12), 
+    new productos("imagenes/Calculator-bro.png","Ibico","Ibico 212X",23.78,meterArray("mesa"),13), 
+    new productos("imagenes/Calculator-bro.png","Ibico","Ibico 1491X",83.78,meterArray("impresora integrada"),14),
+    new productos("imagenes/Calculator-bro.png","Hp","HP 2AP18AA#ABA",129.00,meterArray("cientifica/grafica"),15), 
+    new productos("imagenes/Calculator-bro.png","Hp","HP 50g",321.00,meterArray("cientifica/grafica"),16),
+    new productos("imagenes/Calculator-bro.png","Casio","Casio FX-9750gIII",56.08,meterArray("cientifica/grafica/solar"),17), 
+    new productos("imagenes/Calculator-bro.png","Casio","Casio FX 260 Solar II",56.08,meterArray("cientifica/solar"),18), 
 ];
-
-/*Dibujar los productos en el html*/
-function meterProducto(array){
-    for(let elemento of array){
-        let nuevoProducto = document.createElement("div");
-        nuevoProducto.className = "producto";
-        nuevoProducto.innerHTML = `
-        <div class="contenedorImg">
-            <img src=${elemento.imagen}>
-        </div>
-        <div class="linea">
-            <a class="botonAnadir" id="${elemento.id}">
-                <i class="fa-solid fa-cart-shopping"></i>
-                <p class="letrasAnadirAlCarrito">Anadir al carrito</p>
-            </a>
-        </div>
-        <div class="producto__informacion">
-            <p class="producto__informacion__marca">${elemento.marca}</p>
-            <h2 class="producto__informacion__titulo">${elemento.titulo}</h2>
-            <p class="producto__informacion__precio">U$S${elemento.precio}</p>
-            <a href="#" class="corazon">
-                <i class="fa-regular fa-heart"></i>
-            </a>
-            <!-- <span>
-                <i class="fa-solid fa-heart"></i>
-            </span> -->
-        </div>
-        `;
-        divProductos.appendChild(nuevoProducto);
-    }
-}
-
-meterProducto(listaDeProductos);
 
 /*Ocultar o mostrar el carrito de compra*/
 carrito[0].addEventListener("click",()=>{
@@ -159,4 +128,3 @@ function cargarAlCarrito(){
 
 /* Cuando se cargue el navegador ejecuta esto */
 window.onload = cargarAlCarrito();
-
--- js/codigo.js
+++ js/codigo.js
@@ -2,29 +2,51 @@ let botonesCalculadora = document.getElementsByClassName("botonesCalculadora");
 let inputCalculadora = document.getElementById("inputDatos");
 
 function mostrar(elemento){
-    if(elemento.id == "f6"){
-        inputCalculadora.value = inputCalculadora.value.slice(0, -1)/*Copiar desde la pocision 0 del array hasya la penultima*/
+    let capa;
+    if(elemento.id == "f6"){//borrar un elemento
+        inputCalculadora.value = inputCalculadora.value.slice(0, -1)/*Copiar desde la pocision 0 del array hasta la penultima*/
     }else if(elemento.id == "f5"){
         mostrarResultado();
+    }else if(elemento.id == "f13"){
+        inputCalculadora.value = "";
     }else{
-        inputCalculadora.value += elemento.innerHTML;
+        inputCalculadora.value += elemento.innerHTML;//agregar a value el contenido del a
     }
 }
 
+for(let elemento of botonesCalculadora){
+    elemento.addEventListener("click", () => {mostrar(elemento)})
+}
 
+function esUnSimbolo(elemento,arrayAVer){
+    return arrayAVer.some((elementoDeArray) => elementoDeArray == elemento);
+}
 
 function miPropioSplit(arrayAPicar, arrayDePicacion){
     let numero = "";
     let array = [];
-    numero += arrayAPicar[0];
+    if (arrayAPicar[0] == "("){
+        array.push(arrayAPicar[0]);
+    }else{
+        numero += arrayAPicar[0];
+    }
     for(let elemento in arrayAPicar){
+        /*ignorando el primer elemento pues ya lo agregamos */
         if(parseInt(elemento) === 0){
             continue;
         }
-        let estaElElemento = arrayDePicacion.find((elementoDeArrayDePicacion) => elementoDeArrayDePicacion == arrayAPicar[elemento])
-        if(estaElElemento != undefined){
-            if(arrayDePicacion.some((elementoDelArray) => elementoDelArray == arrayAPicar[elemento - 1]) ){
-                numero += arrayAPicar[elemento];
+        /*ALGORITMO PARA QUE FUNCIONE (1+1)*((2--3)/(-4+6)) Y -3+2 */
+        if(esUnSimbolo(arrayAPicar[elemento],arrayDePicacion)){
+            if(esUnSimbolo(arrayAPicar[elemento - 1],arrayDePicacion)){// - -4 
+                if(arrayAPicar[elemento] == "-" || arrayAPicar[elemento] == "+"){
+                    if(arrayAPicar[elemento-1] == ")"){
+                        array.push(arrayAPicar[elemento]);
+                    }else{
+                        numero += arrayAPicar[elemento];
+                    }
+                }else{
+                    array.push(arrayAPicar[elemento]);
+                }
             }else{
                 array.push(numero);
                 array.push(arrayAPicar[elemento]);
@@ -34,7 +56,9 @@ function miPropioSplit(arrayAPicar, arrayDePicacion){
             numero += arrayAPicar[elemento];
         }
     }
-    array.push(numero);
+    if (numero != ""){
+        array.push(numero);
+    }
     return array
 }
 
@@ -42,14 +66,14 @@ const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
 const multiplicacion = (a, b) => a * b;
 const division = (a, b) => a / b;
+const modulo = (a, b) => a % b;
+const potencia = (a, b) => Math.pow(a, b);//base y exponente
 
 function queOperacionUsar(a,b,funcion){
     return funcion(a,b);
 }
 
-function mostrarResultado(){
-    let inputOperacion = inputCalculadora.value;
-    let arrayDeOperacion = miPropioSplit(inputOperacion, ["+","-","*","/"]);
+function calcularPedacito(arrayDeOperacion){
     let operacion;
     let a;
     let b;
@@ -71,15 +95,56 @@ function mostrarResultado(){
             operacion = division;
             a = arrayDeOperacion[0];
             b = arrayDeOperacion[2];
+        }else if(arrayDeOperacion[1] == "%"){
+            operacion = modulo;
+            a = arrayDeOperacion[0];
+            b = arrayDeOperacion[2];
+        }else if(arrayDeOperacion[1] == "^"){
+            operacion = potencia;
+            a = arrayDeOperacion[0];
+            b = arrayDeOperacion[2];
         }
         resultado = queOperacionUsar(parseInt(a),parseInt(b),operacion);
-        arrayDeOperacion.shift();
+        arrayDeOperacion.shift(); //elimina el primer elemento
         arrayDeOperacion.shift();
         arrayDeOperacion[0] = resultado;
     }
+    return resultado;
+}
+
+function desapilar(array){
+    let arrayDeCalculo = [];
+    while(array[array.length-1] != "("){
+        arrayDeCalculo.unshift(array[array.length-1]);
+        array.pop();
+    }
+    array.pop();
+    return arrayDeCalculo;
+}
+
+function calcular(array){
+    let pila = [];
+    let resultado = 0;
+    for(let elemento of array){
+        if(elemento != ")"){
+            pila.push(elemento);
+            console.log(pila);
+        }else{
+            let arrayACalcular = desapilar(pila);
+            pila.push(calcularPedacito(arrayACalcular));
+            console.log(pila);
+        }
+    }
+    resultado = calcularPedacito(pila);
+    return resultado;
+}
+
+function mostrarResultado(){
+    let inputOperacion = inputCalculadora.value;
+    let arrayDeOperacion = miPropioSplit(inputOperacion, ["+","-","*","/","^","%","(",")"]);
+    console.log(arrayDeOperacion);
+    let resultado = calcular(arrayDeOperacion);
+
     inputCalculadora.value = resultado;
 }
 
-for(let elemento of botonesCalculadora){
-    elemento.addEventListener("click", () => {mostrar(elemento)})
-}
